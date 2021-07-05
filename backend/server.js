6274a2181ff242e343e48c8595a1c6ca660926bd
Abdullah0322 <abdullahnaveed71.am@gmail.com>
2021-07-05 15:04:17 +0500

    ok

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,6 @@ app.use('/api/', emailRoutes)
 
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
@@ -40,10 +39,10 @@ if (process.env.NODE_ENV === 'production') {
   // app.get('*', (req, res) =>
   //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   // )
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running....')
+//   })
 }
 
 app.use(notFound)
